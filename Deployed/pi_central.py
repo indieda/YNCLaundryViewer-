be70fa34b3a4f62f6546fb22eb30b0fa6d6b5c42
Chua Yun Da <yd.chua@u.yale-nus.edu.sg>
2020-09-23 04:55:04 +0800

    Fixed bluetooth transmission issues and validated them on 2 arduinos, on a new set based python data model

--- Deployed/pi_central.py
+++ Deployed/pi_central.py
@@ -3,6 +3,7 @@
 from bluepy import btle
 from time import sleep
 from func_timeout import func_timeout, FunctionTimedOut
+from func_timeout.StoppableThread import StoppableThread
 from datetime import datetime
 import time
 import threading
@@ -29,7 +30,7 @@ url = "https://enrixpn98m8gp.x.pipedream.net"
 cendana_addr = ['ec:24:b8:23:78:29','58:7A:62:17:B8:07','D4:F5:13:FD:C6:73','EC:24:B8:21:BA:1B','D4:F5:13:FD:C9:D7','D4:F5:13:FD:DD:51']
 #ec:24 is washer 6.
 washer_addr_reversed = [6,5,4,3,2,1]
-washer_state_array = [{},{},{},{},{},{}]
+washer_state_array = [set(),set(),set(),set(),set(),set()]
 college = "Cendana"
 time_prev = time.time()
 time_exit = time.time()
@@ -51,18 +52,7 @@ def telegram_bot(msg):
         except:
             pass
 
-def read_ble(ble_no,i):
-    try:
-        conn = btle.Peripheral(ble_no)
-        data = conn.readCharacteristic(0x0025)
-    #print(data.decode('utf8')+str(i))
-        read_ble.data_decode = data.decode('utf8')
-        conn.disconnect()
-        #read_ble.data_decode = data_decode
-    except Exception as e:
-        #conn.disconnect()
-        read_ble.data_decode = "nil"
-        pass
+
 
 def write_log(message):
     global f
@@ -87,10 +77,10 @@ def upload_to_web():
             if "on" in d:
             #idx = data_decoded[1]
                 #response = requests.post(url,json={"Washer {} ble message: {}".format(idx,d):"Couldn't read..."})
-                response = requests.post(url , json = {"Washer {}".format(washer_addr_reversed[idx]):"On, machine is not available for use"})
+                #response = requests.post(url , json = {"Washer {}".format(washer_addr_reversed[idx]):"On, machine is not available for use"})
                 resp = requests.post(ync_url , json = {"sensorValue":188,"college":"Cendana","machineLabel":"Washer_{}".format(washer_addr_reversed[idx])})
-                resp = requests.post(new_url , json = {"sensorValue":188,"college":"Cendana","machineLabel":"Washer_{}".format(washer_addr_reversed[idx])})
-                resp2 = requests.post(test_url , json = {"sensorValue":188,"college":"Cendana","machineLabel":"Washer_{}".format(washer_addr_reversed[idx])})
+                #resp = requests.post(new_url , json = {"sensorValue":188,"college":"Cendana","machineLabel":"Washer_{}".format(washer_addr_reversed[idx])})
+                #resp2 = requests.post(test_url , json = {"sensorValue":188,"college":"Cendana","machineLabel":"Washer_{}".format(washer_addr_reversed[idx])})
                 if l != "n":
                     #tele = telegram_bot("Washer {} ".format(washer_addr_reversed[idx]) + d)
                     pass
@@ -103,10 +93,10 @@ def upload_to_web():
                 #d[1] == "f"
             elif  "off" in d:
                 #idx = data_decoded[1]
-                response = requests.post(url, json = {"Washer {}".format(washer_addr_reversed[idx]):"Off, machine is available for use"})
+                #response = requests.post(url, json = {"Washer {}".format(washer_addr_reversed[idx]):"Off, machine is available for use"})
                 resp = requests.post(ync_url , json = {"sensorValue":888,"college":"Cendana","machineLabel":"Washer_{}".format(washer_addr_reversed[idx])})
-                resp = requests.post(new_url , json = {"sensorValue":888,"college":"Cendana","machineLabel":"Washer_{}".format(washer_addr_reversed[idx])})
-                resp2 = requests.post(test_url , json = {"sensorValue":888,"college":"Cendana","machineLabel":"Washer_{}".format(washer_addr_reversed[idx])})
+                #resp = requests.post(new_url , json = {"sensorValue":888,"college":"Cendana","machineLabel":"Washer_{}".format(washer_addr_reversed[idx])})
+                #resp2 = requests.post(test_url , json = {"sensorValue":888,"college":"Cendana","machineLabel":"Washer_{}".format(washer_addr_reversed[idx])})
                 #response2 = requests.post(ync_url , json = {'Washer 6':'Off'})
                 if l != "f":
                     #tele = telegram_bot("Washer {} ".format(washer_addr_reversed[idx]) + d)
@@ -118,10 +108,10 @@ def upload_to_web():
                 print('Washer {}'.format(idx), d ,"and Uploaded")
                 #d[1] == "r"
             elif "error" in d:
-                response = requests.post(url, json = {'Washer 6':'Error, the lock light is blinking!'})
+                #response = requests.post(url, json = {'Washer 6':'Error, the lock light is blinking!'})
                 resp = requests.post(ync_url , json = {"sensorValue":2000,"college":"Cendana","machineLabel":"Washer_{}".format(washer_addr_reversed[idx])})
-                resp = requests.post(new_url , json = {"sensorValue":2000,"college":"Cendana","machineLabel":"Washer_{}".format(washer_addr_reversed[idx])})
-                response2 = requests.post(test_url , json = {'Washer 6':'Error'})
+                #resp = requests.post(new_url , json = {"sensorValue":2000,"college":"Cendana","machineLabel":"Washer_{}".format(washer_addr_reversed[idx])})
+                #response2 = requests.post(test_url , json = {'Washer 6':'Error'})
                 write_log("Washer {} Error".format(washer_addr_reversed[idx]))
                 print('Washer {}'.format(idx), d ,"and Uploaded")
                 if l == "r":
@@ -139,7 +129,7 @@ def upload_to_web():
             else:
                 #kill[idx-1] = kill[idx-1] + 1
                 pass
-            washer_state_array[idx] = {}
+            washer_state_array[idx] = set()
     except Exception as e:
         #print(e)
         write_log(e)
@@ -180,41 +170,83 @@ finally:
 #    telegram_bot("turning python script on - a")
 #except:
 #    pass
+def read_ble(ble_no,i):
+    try:
+        print(i)
+        conn = btle.Peripheral(ble_no, timeout=3.0)
+        #This 0.8 seconds is really important, because it takes time for the connection data to load into the classes.
+        #If we don't add it in, the likelihood of it returning "b'"x01\x02\x03\x04\x05\x00\x00\x00\x00\x00 is super high!!!""
+        sleep(0.8)
+        data = conn.readCharacteristic(0x0025)
+        print("connected")
+    #print(data.decode('utf8')+str(i))
+        read_ble.data_decode = data.decode('utf8')
+        print(read_ble.data_decode)
+        washer_state_array[i].add(read_ble.data_decode)
+        conn.disconnect()
+        #read_ble.data_decode = data_decode
+    except Exception as e:
+        #conn.disconnect()
+        print(e)
+        read_ble.data_decode = "nil"
+        return Exception
+
+def functime(fun, addr,to, idxx):
+    func_timeout(timeout=to,func=fun,args=(addr_i,idxx))
+
+
+i=0
+global lll
+heh = threading.Lock()
+lll = [threading.Thread(),threading.Thread(),threading.Thread(),threading.Thread(),threading.Thread(),threading.Thread()]
 while (time.time() - time_exit < 575):
-    i=0
     try:
         for idxx,addr_i in enumerate(cendana_addr, 0):
             try:
-                #func_timeout(0.7,read_ble,args=(addr_i,i))
-                func_timeout(1.0,read_ble,args=(addr_i,i))
-                if ((read_ble.data_decode[1] == "n") or (read_ble.data_decode[1] == "f") or (read_ble.data_decode[1] == "r") or (read_ble.data_decode == "first") or (read_ble.data_decode == "second") or (read_ble.data_decode == "third") or (read_ble.data_decode == "fourth")or (read_ble.data_decode == "fifth") or (read_ble.data_decode == "sixth") or (read_ble.data_decode == "seventh") or (read_ble.data_decode == "eigth") or (read_ble.data_decode == "ninth") or (read_ble.data_decode == "tenth") or (read_ble.data_decode == "max")):
-                    washer_state_array[idxx].add(read_ble.data_decode)
-                    print(read_ble.data_decode+str(washer_addr_reversed[idxx]))
-                    print(datetime.now())
-                    write_log(read_ble.data_decode+str(washer_addr_reversed[idxx])+" "+datetime.now())
-                else:
-                    pass
+                with heh:
+                    lll[idxx] = threading.Thread(target=read_ble,args=(addr_i,idxx))
+                    lll[idxx].start()
+                #func_timeout(timeout=0.7,func=read_ble,args=(addr_i,idxx))
+                #func_timeout(timeout=0.7,func=read_ble,args=(addr_i,idxx))
+                #lll[idx] = func_timeout(timeout=0.7,func=read_ble,args=(addr_i,idxx))
+                #lll[idxx].retry(timeout=2.0)
+                
+                    if ((read_ble.data_decode[1] == "n") or (read_ble.data_decode[1] == "f") or (read_ble.data_decode[1] == "r") or (read_ble.data_decode == "first") or (read_ble.data_decode == "second") or (read_ble.data_decode == "third") or (read_ble.data_decode == "fourth")or (read_ble.data_decode == "fifth") or (read_ble.data_decode == "sixth") or (read_ble.data_decode == "seventh") or (read_ble.data_decode == "eigth") or (read_ble.data_decode == "ninth") or (read_ble.data_decode == "tenth") or (read_ble.data_decode == "max")):
+                        washer_state_array[idxx].add(read_ble.data_decode)
+                        print(read_ble.data_decode+str(washer_addr_reversed[idxx]))
+                        print(datetime.now())
+                        write_log(read_ble.data_decode+str(washer_addr_reversed[idxx])+" "+datetime.now())
+                    else:
+                        pass
             #upload = threading.Thread(target=stdhandle, args = (read_ble.data_decode,i), daemon = True)
             except Exception as e:
-                #print(e)
+                print(e)
+                lll[idxx].join()
                 pass
             except FunctionTimedOut as e:
-                #print(e)
+                print(e)
+                lll[idxx].join()
                 pass
             time_elapsed = time.time() - time_prev
+        print("stopped")
+        lll[idxx].join()
             #if kill[i] > 30:
                 #kill = [0,0]
                 #print("restarting program")
                 #os.fsync(fd)
                 #os.execv(__file__, sys.argv)
-        if (time_elapsed > 10):
-            upload_to_web()
+        if (time_elapsed > 18):
+            y = threading.Thread(upload_to_web())
+            y.start()
             time_prev = time.time()
+            y.join()
         i = i+1
         sleep(0.8)
+        print(i)
     except Exception as e:
-        #print(e)
+        print(e)
         pass
+
 
 
 try:
@@ -232,5 +264,4 @@ except:
 finally:
     uptime_log.close()
 
-exit()
-
+#exit()
